72440c15dd47af2513ccb9f6f7ceb052e561471d
mehdi <mehdibs313@gmail.com>
2021-04-03 00:37:41 +0430

    Requirements And Create Model Admin Of Tables

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,19 @@
 from django.contrib import admin
-
-# Register your models here.
 from .models import *
-admin.site.register(UserProfile)
-admin.site.register(Article)
-admin.site.register(Category)
+
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ['user','avatar','description']
+
+
+class ArticleAdmin(admin.ModelAdmin):
+    search_fields = ['title','content']
+    list_display = ['title','category','created_at']
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['title','cover']
+
+
+admin.site.register(UserProfile,UserProfileAdmin)
+admin.site.register(Article,ArticleAdmin)
+admin.site.register(Category,CategoryAdmin)
