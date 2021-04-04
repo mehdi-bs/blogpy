34d2b2c3175de9565c768b0e2bc5d555e3aa31cb
mehdi <mehdibs313@gmail.com>
2021-04-04 23:13:20 +0430

    Set Static File Settings And StaticFiles

--- blogpy/settings.py
+++ blogpy/settings.py
@@ -124,8 +124,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR,'blog/static'),
+)
 
 # Ckeditor
 CKEDITOR_BASEPATH = "/static/ckeditor/ckeditor/"
