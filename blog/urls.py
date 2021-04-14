79227909b390ea16d7a383358661bf5ab4cb944e
mehdi <mehdibs313@gmail.com>
2021-04-14 23:36:35 +0430

    Add rest framework And Add Api For List Articles

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$',views.Index.as_view(),name='index'),
-    url(r'^contact/$',views.ContactPage.as_view(),name='contact')
+    url(r'^contact/$',views.ContactPage.as_view(),name='contact'),
+    url(r'^article/all/$',views.AllArticleAPIView.as_view())
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 from .models import *
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
 class Index(TemplateView):
     def get(self, request, **kwargs):
@@ -38,3 +41,27 @@ class Index(TemplateView):
 
 class ContactPage(TemplateView):
     template_name = 'contact.html'
+
+
+class AllArticleAPIView(APIView):
+
+    def get(self,request,format=None):
+        try:
+            data = []
+            all_articles = Article.objects.all().order_by('-created_at')[:9]
+
+            for article in all_articles:
+                data.append({
+                    'title':article.title,
+                    'cover':article.cover.url,
+                    'content':article.content,
+                    'created_at':article.created_at,
+                    'category':article.category.title,
+                    'author':article.author.user.first_name + ' ' + article.author.user.last_name,
+                    'promote':article.promote,
+                })
+
+            return Response({'data':data},status.HTTP_200_OK)
+
+        except:
+            return Response({'error': "Retriving All Articles Failed!"}, status.HTTP_500_INTERNAL_SERVER_ERROR)
